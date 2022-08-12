451c40b11a14814594a53ac5059daf26e02bd131
CXPhoenix <jate9229@gmail.com>
2022-08-12 14:01:24 +0800

    fix: 🐛 props data fixed

--- src/components/NoticeModel.vue
+++ src/components/NoticeModel.vue
@@ -6,12 +6,9 @@
       class="flex max-h-fit flex-col items-center justify-center gap-10 overflow-auto rounded-lg border-8 border-s-yello bg-white p-4 md:max-w-xl"
     >
       <slot>特別注意</slot>
-      <!-- <div class="flex items-center justify-center">
-        <input type="checkbox" name="noShow" id="noShow" v-bind="noShow" />
-        下次不要再出現
-      </div> -->
+
       <button
-        @click="next"
+        @click="closeModel"
         class="rounded-md bg-s-green py-2 px-4 text-center text-white"
       >
         繼續瀏覽新生專區
@@ -23,19 +20,12 @@
 <script>
 import { onMounted, reactive, ref } from "vue";
 export default {
-  props: { adData: Object, closeModel: Function },
+  props: { closeModel: Function },
 
-  setup(props, { emit }) {
-    const noShow = ref(false);
-    const { adData, closeModel } = props;
-    const next = () => {
-      if (noShow) {
-        emit("noShowAgain", adData.title);
-      }
-      closeModel();
-    };
+  setup(props) {
+    const { closeModel } = props;
 
-    return { adData, noShow, next };
+    return { closeModel };
   },
 };
 </script>
